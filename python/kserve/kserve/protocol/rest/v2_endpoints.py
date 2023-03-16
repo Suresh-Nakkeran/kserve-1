f598a1050fd32aadd297f6303b479b2d1a6822d4
Suresh-Nakkeran <suresh.n@ideas2it.com>
2023-03-16 16:50:44 +0530

    Added request id in v2 protocol payload
    Signed-off-by: Suresh-Nakkeran <suresh.n@ideas2it.com>

--- python/kserve/kserve/protocol/rest/v2_endpoints.py
+++ python/kserve/kserve/protocol/rest/v2_endpoints.py
@@ -126,7 +126,7 @@ class V2Endpoints:
         request_headers = dict(raw_request.headers)
         infer_inputs = [InferInput(name=input.name, shape=input.shape, datatype=input.datatype,
                                    data=input.data) for input in request_body.inputs]
-        infer_request = InferRequest(model_name=model_name, infer_inputs=infer_inputs)
+        infer_request = InferRequest(request_id=request_body.id, model_name=model_name, infer_inputs=infer_inputs)
         response, response_headers = await self.dataplane.infer(
             model_name=model_name, body=infer_request, headers=request_headers)
 
